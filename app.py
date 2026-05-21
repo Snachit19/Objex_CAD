a10fdb582cc4415607cb976e3442970ca6691ad9
Tanmay Singhal <code_machine@Tanmays-MacBook-Pro.local>
2026-05-21 14:28:08 +0545

    Uswe login page layout

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="public")
 CORS(app)
 
 
 @app.route("/")
 def home():
-    return "Objex CAD Flask server is running."
+    return send_from_directory("public", "index.html")
+
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory("public", path)
 
 
 if __name__ == "__main__":
